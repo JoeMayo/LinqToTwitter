45408b840e1418ab4c2023da2f5665f1823d82d1
JoeMayo <none@none.com>
2010-03-21 03:39:15 +0000

    Moved files into folders for better project organization

---
+++ LinqToTwitter/Geo/GeoType.cs
@@ -0,0 +1,18 @@
+﻿namespace LinqToTwitter
+{
+    /// <summary>
+    /// For working with Twitter Geo places
+    /// </summary>
+    public enum GeoType
+    {
+        /// <summary>
+        /// Get a list of valid places (from Twitter) that can be attached to an Update
+        /// </summary>
+        Reverse,
+
+        /// <summary>
+        /// Get more details on a place (found via GeoType.Reverse)
+        /// </summary>
+        ID
+    }
+}
