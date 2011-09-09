b1a6e06ac74e7d2cf6c916672c18b9c3fd1f8414
IDisposable <none@none.com>
2011-09-09 23:38:39 +0000

    Part 4 of 4. Complete transition to Json-only endpoints for the Trends calls: Created a LinqToTwitterJson project, moved the extension methods there. Moved all the Json "dto" object and serialization converters to there.

--- LinqToTwitterAg/Trend/TrendRequestProcessor.cs
+++ LinqToTwitterAg/Trend/TrendRequestProcessor.cs
@@ -240,10 +240,8 @@ namespace LinqToTwitter
 
         private IEnumerable<Trend> HandleDailyWeeklyResponse(string responseJson)
         {
-            var converter = new Converter();
-            var jss = new JavaScriptSerializer();
-            jss.RegisterConverters(new JavaScriptConverter[] { converter });
-            var period = jss.Deserialize<JsonDailyWeeklyTrends>(responseJson);
+            var serializer = Json.TrendConverter.GetSerializer();
+            var period = serializer.Deserialize<Json.DailyWeeklyTrends>(responseJson);
             var asOf = XTwitterElement.EpochBase + TimeSpan.FromSeconds(period.as_of);
             var emptyLocations = new List<Location>();
             var flat = from slot in period.slots
@@ -275,14 +273,12 @@ namespace LinqToTwitter
         private IEnumerable<Trend> HandleLocationResponse(string responseJson)
         {
             var now = DateTime.UtcNow;
-            var converter = new Converter();
-            var jss = new JavaScriptSerializer();
-            jss.RegisterConverters(new JavaScriptConverter[] { converter });
-            var responses = jss.Deserialize<JsonTrends[]>(responseJson);
+            var serializer = Json.TrendConverter.GetSerializer();
+            var responses = serializer.Deserialize<Json.Trends[]>(responseJson);
             var flat = from response in responses
                        let asOf = response.as_of.GetDate(now)
                        let locations = (from place in response.locations
-                                        select place.ToLocation()).ToList()
+                                        select ToLocation(place)).ToList()
                        let trends = (from trend in response.trends
                                      select new Trend
                                      {
@@ -309,17 +305,16 @@ namespace LinqToTwitter
 
         private IEnumerable<Trend> HandleAvailableResponse(string responseJson)
         {
-            var asOf = DateTime.UtcNow;
-            var converter = new Converter();
-            var jss = new JavaScriptSerializer();
-            jss.RegisterConverters(new JavaScriptConverter[] { converter });
-            var places = jss.Deserialize<JsonPlace[]>(responseJson);
+            var serializer = Json.TrendConverter.GetSerializer();
+            var places = serializer.Deserialize<Json.Place[]>(responseJson);
             var locations = new List<Location>();
             foreach (var place in places)
             {
-                var location = place.ToLocation();
+                var location = ToLocation(place);
                 locations.Add(location);
             }
+
+            var asOf = DateTime.UtcNow;
 
             // we fake a single Trend to hang the locations off of...
             yield return new Trend
@@ -339,250 +334,20 @@ namespace LinqToTwitter
                 Locations = locations
             };
         }
-    }
 
-    internal class Converter : JavaScriptConverter
-    {
-        public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
+        internal Location ToLocation(Json.Place place)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
-        }
-
-        public override IEnumerable<Type> SupportedTypes
-        {
-            get
-            {
-                return new List<Type> 
-                {
-                    typeof(JsonDailyWeeklyTrends), 
-                    typeof(JsonTrends),
-                    typeof(JsonSlottedTrend),
-                    typeof(JsonTrend),
-                    typeof(JsonPlace),
-                    typeof(JsonPlaceType)
-                };
-            }
-        }
-
-        public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
-        {
-            if (dictionary == null)
-                throw new ArgumentNullException("dictionary");
-
-            if (type == typeof(JsonDailyWeeklyTrends))
-            {
-                return JsonDailyWeeklyTrends.Deserialize(dictionary, serializer);
-            }
-            else if (type == typeof(JsonSlottedTrend))
-            {
-                return JsonSlottedTrend.Deserialize(dictionary, serializer);
-            }
-            else if (type == typeof(JsonTrends))
-            {
-                return JsonTrends.Deserialize(dictionary, serializer);
-            }
-            else if (type == typeof(JsonTrend))
-            {
-                return JsonTrend.Deserialize(dictionary, serializer);
-
-            }
-            else if (type == typeof(JsonPlace))
-            {
-                return JsonPlace.Deserialize(dictionary, serializer);
-            }
-            else if (type == typeof(JsonPlaceType))
-            {
-                return JsonPlaceType.Deserialize(dictionary, serializer);
-            }
-            else
-            {
-                // what?
-                return null;
-            }
-        }
-    }
-
-    [DataContract]
-    public class JsonDailyWeeklyTrends
-    {
-        [DataMember]
-        public ulong as_of { get; set; } // epoch seconds
-
-        [DataMember]
-        public JsonSlottedTrend[] slots { get; set; }
-
-        public static JsonDailyWeeklyTrends Deserialize(IDictionary<string, object> dictionary, JavaScriptSerializer serializer)
-        {
-            var trends = dictionary["trends"] as Dictionary<string, object>;
-            var asOfEpoch = (int)dictionary["as_of"];
-            var trendSlots = from trend in trends
-                             let slotTime = trend.Key
-                             let trendArray = serializer.ConvertToType<JsonTrend[]>(trend.Value)
-                             select new JsonSlottedTrend
-                             {
-                                 time_slot = slotTime,
-                                 trends = trendArray
-                             };
-
-            return new JsonDailyWeeklyTrends
-            {
-                as_of = (ulong)asOfEpoch,
-                slots = trendSlots.ToArray()
-            };
-        }
-    }
-
-    [DataContract]
-    public class JsonSlottedTrend
-    {
-        [DataMember]
-        public string time_slot { get; set; }
-
-        [DataMember]
-        public JsonTrend[] trends { get; set; }
-
-        internal static JsonSlottedTrend Deserialize(IDictionary<string, object> dictionary, JavaScriptSerializer serializer)
-        {
-            // THIS IS WRONG, fortunately we never need to do this...
-            var slot = dictionary.First();
-            var slotTime = slot.Key;
-            var trendArray = serializer.ConvertToType<JsonTrend[]>(slot.Value);
-            return new JsonSlottedTrend
-            {
-                time_slot = slotTime,
-                trends = trendArray
-            };
-        }
-    }
-
-    [DataContract]
-    public class JsonTrends
-    {
-        [DataMember]
-        public JsonTrend[] trends { get; set; }
-        [DataMember]
-        public string created_at { get; set; }
-        [DataMember]
-        public string as_of { get; set; }
-        [DataMember]
-        public JsonPlace[] locations { get; set; }
-
-        internal static JsonTrends Deserialize(IDictionary<string, object> dictionary, JavaScriptSerializer serializer)
-        {
-            var jsonTrends = dictionary["trends"] as ArrayList;    // required!
-            var createdAt = dictionary.GetValue<string>("created_at");
-            var asOf = dictionary.GetValue<string>("as_of");
-            var locations = dictionary.GetValue<ArrayList>("locations");
-            var locs = (from object location in locations
-                        select serializer.ConvertToType<JsonPlace>(location));
-            var trds = (from object trend in jsonTrends
-                               select serializer.ConvertToType<JsonTrend>(trend));
-            return new JsonTrends
-            {
-                as_of = asOf,
-                created_at = createdAt,
-                locations = locs.ToArray(),
-                trends = trds.ToArray()
-            };
-        }
-    }
-
-    [DataContract]
-    public class JsonTrend
-    {
-        [DataMember]
-        public string query { get; set; }
-        [DataMember]
-        public string name { get; set; }
-        [DataMember]
-        public string url { get; set; }
-        [DataMember]
-        public string events { get; set; }  // no idea
-        [DataMember]
-        public string promoted_content { get; set; } // not sure
-
-        internal static JsonTrend Deserialize(IDictionary<string, object> dictionary, JavaScriptSerializer serializer)
-        {
-            return new JsonTrend
-            {
-                name = dictionary["name"] as string, // required!
-                query = dictionary.GetValue<string>("query"),
-                url = dictionary.GetValue<string>("url"),
-                events = dictionary.GetValue<string>("events"),
-                promoted_content = dictionary.GetValue<string>("promoted_content")
-            };
-        }
-    }
-
-    [DataContract]
-    public class JsonPlaceType
-    {
-        [DataMember]
-        public string name { get; set; }
-        [DataMember]
-        public int code { get; set; }
-
-        internal static JsonPlaceType Deserialize(IDictionary<string, object> dictionary, JavaScriptSerializer serializer)
-        {
-            return new JsonPlaceType
-            {
-                name = dictionary["name"] as string,    // required!
-                code = dictionary.GetValue<int>("code")
-            };
-        }
-    }
-
-    [DataContract]
-    public class JsonPlace
-    {
-        [DataMember]
-        public string name { get; set; }
-        [DataMember]
-        public string url { get; set; }
-        [DataMember]
-        public ulong woeid { get; set; }
-        [DataMember]
-        public ulong parentid { get; set; }
-        [DataMember]
-        public JsonPlaceType placeType { get; set; }
-        [DataMember]
-        public string country { get; set; }
-        [DataMember]
-        public string countryCode { get; set; }
-
-        internal static JsonPlace Deserialize(IDictionary<string, object> dictionary, JavaScriptSerializer serializer)
-        {
-            var placeName = dictionary["name"] as string; // required!
-            var pts = dictionary.GetValue<object>("placeType");
-            var pt = serializer.ConvertToType<JsonPlaceType>(pts);
-            var woeId = dictionary.GetValue<int>("woeid");
-            var parentId = dictionary.GetValue<int>("parentid");
-            return new JsonPlace
-            {
-                name = placeName,
-                url = dictionary.GetValue<string>("url"),
-                woeid = (ulong)woeId,
-                parentid = (ulong)parentId,
-                placeType = pt,
-                country = dictionary.GetValue<string>("country"),
-                countryCode = dictionary.GetValue<string>("countryCode")
-            };
-        }
-        
-        internal Location ToLocation()
-        {
-            var pt = this.placeType ?? new JsonPlaceType { code = 0, name = string.Empty };
+            var pt = place.placeType ?? new Json.PlaceType { code = 0, name = string.Empty };
             return new Location
             {
-                Country = this.country ?? string.Empty,
-                CountryCode = this.countryCode ?? string.Empty,
-                Name = this.name ?? string.Empty,
-                ParentID = this.parentid.ToString(CultureInfo.InvariantCulture),
+                Country = place.country ?? string.Empty,
+                CountryCode = place.countryCode ?? string.Empty,
+                Name = place.name ?? string.Empty,
+                ParentID = place.parentid.ToString(CultureInfo.InvariantCulture),
                 PlaceTypeName = pt.name,
                 PlaceTypeNameCode = pt.code,
-                Url = this.url ?? string.Empty,
-                WoeID = this.woeid.ToString(CultureInfo.InvariantCulture)
+                Url = place.url ?? string.Empty,
+                WoeID = place.woeid.ToString(CultureInfo.InvariantCulture)
             };
         }
     }
