1ddb37f9d016fdf2bdd0a6ec9ec5bb1de4436866
JoeMayo <none@none.com>
2011-11-29 19:50:17 +0000

    Added example of OAuth with status update to Windows Phone Demo.

--- LinqToTwitterWP/Properties/AssemblyInfo.cs
+++ LinqToTwitterWP/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LinqToTwitterWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LINQ to Twitter for Windows Phone")]
+[assembly: AssemblyDescription("Open Source LINQ Provider for the Twitter Micro-Blogging Service (Windows Phone)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LinqToTwitterWP")]
+[assembly: AssemblyCompany("Joe Mayo")]
+[assembly: AssemblyProduct("LINQ to Twitter for Windows Phone")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.22")]
+[assembly: AssemblyFileVersion("2.0.22")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
---
+++ WindowsPhoneDemo/OAuth.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Navigation;
+using LinqToTwitter;
+using Microsoft.Phone.Controls;
+
+namespace WindowsPhoneDemo
+{
+    public partial class OAuth : PhoneApplicationPage
+    {
+        PinAuthorizer pinAuth;
+
+        public OAuth()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(Page_Loaded);
+            OAuthWebBrowser.LoadCompleted += new System.Windows.Navigation.LoadCompletedEventHandler(OAuthWebBrowser_LoadCompleted);
+        }
+
+        void OAuthWebBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            EnterPinTextBlock.Visibility = Visibility.Visible;
+            PinTextBox.IsEnabled = true;
+            AuthenticateButton.IsEnabled = true;
+        }
+
+        void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.pinAuth = new PinAuthorizer
+            {
+                Credentials = new InMemoryCredentials
+                {
+                    ConsumerKey = "",
+                    ConsumerSecret = ""
+                },
+                UseCompression = true,
+                GoToTwitterAuthorization = pageLink =>
+                    Dispatcher.BeginInvoke(() => OAuthWebBrowser.Navigate(new Uri(pageLink, UriKind.Absolute)))
+            };
+
+            this.pinAuth.BeginAuthorize(resp =>
+                Dispatcher.BeginInvoke(() =>
+                {
+                    switch (resp.Status)
+                    {
+                        case TwitterErrorStatus.Success:
+                            break;
+                        case TwitterErrorStatus.TwitterApiError:
+                        case TwitterErrorStatus.RequestProcessingException:
+                            MessageBox.Show(
+                                resp.Error.ToString(),
+                                resp.Message,
+                                MessageBoxButton.OK);
+                            break;
+                    }
+                }));
+        }
+
+        private void AuthenticateButton_Click(object sender, RoutedEventArgs e)
+        {
+            pinAuth.CompleteAuthorize(
+                PinTextBox.Text,
+                completeResp => Dispatcher.BeginInvoke(() =>
+                {
+                    switch (completeResp.Status)
+                    {
+                        case TwitterErrorStatus.Success:
+                            SharedState.Authorizer = pinAuth;
+                            NavigationService.GoBack();
+                            break;
+                        case TwitterErrorStatus.TwitterApiError:
+                        case TwitterErrorStatus.RequestProcessingException:
+                            MessageBox.Show(
+                                completeResp.Error.ToString(),
+                                completeResp.Message,
+                                MessageBoxButton.OK);
+                            break;
+                    }
+                }));
+        }
+    }
+}
