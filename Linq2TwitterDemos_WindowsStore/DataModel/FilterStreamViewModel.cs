705090d12e8c11d8e6b12b82cb2a908e16401156
JoeMayo <jmayo@mayosoftware.com>
2013-11-11 16:40:48 -0800

    Added streaming to Windows Store demos.

---
+++ Linq2TwitterDemos_WindowsStore/DataModel/FilterStreamViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using LinqToTwitter;
+
+namespace Linq2TwitterDemos_WindowsStore.DataModel
+{
+    class FilterStreamViewModel : StreamViewModel
+    {
+        public override async void OnStart(object obj)
+        {
+            int count = 0;
+
+            var twitterCtx = new TwitterContext(SharedState.Authorizer);
+
+            await
+                (from strm in twitterCtx.Streaming
+                 where strm.Type == StreamingType.Filter &&
+                       strm.Track == "twitter"
+                 select strm)
+                .StartAsync(async strm =>
+                {
+                    Show(strm.Content);
+
+                    if (count++ >= 5)
+                        strm.CloseStream();
+                });
+        }
+    }
+}
