55deea7b59b0c089da6df2632b0e826426675747
JoeMayo <jmayo@mayosoftware.com>
2014-02-23 18:18:52 -0800

    Fixed exception handling for ExecuteTwitterStream.

--- LinqToTwitterCP/Properties/AssemblyInfo.cs
+++ LinqToTwitterCP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Joe Mayo")]
 [assembly: AssemblyProduct("LINQ to Twitter - Client Profile")]
-[assembly: AssemblyCopyright("Copyright ©  2008 - 2013")]
+[assembly: AssemblyCopyright("Copyright ©  2008 - 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.11.0")]
-[assembly: AssemblyFileVersion("2.1.11.0")]
+[assembly: AssemblyVersion("2.1.13.0")]
+[assembly: AssemblyFileVersion("2.1.13.0")]
 
 [assembly: InternalsVisibleTo(
     "LinqToTwitterXUnitTests, PublicKey=" +
