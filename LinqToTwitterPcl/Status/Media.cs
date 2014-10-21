2b1ad6c4859cc6e62039de22041ac10c8d746425
Joe Mayo <jmayo@mayosoftware.com>
2014-10-20 22:25:32 -0700

    Added Media class which should have been added via earlier commit.

---
+++ LinqToTwitterPcl/Status/Media.cs
@@ -0,0 +1,27 @@
+﻿using LitJson;
+using System;
+using LinqToTwitter.Common;
+
+namespace LinqToTwitter
+{
+    public class Media
+    {
+        public Media() { }
+        public Media(JsonData media)
+        {
+            if (media == null) return;
+
+            Image = new MediaImage(media.GetValue<JsonData>("image"));
+            MediaID = media.GetValue<string>("media_id_string").GetULong(0);
+            Size = media.GetValue<int>("size");
+        }
+
+        public MediaType Type { get; set; }
+
+        public MediaImage Image { get; set; }
+
+        public ulong MediaID { get; set; }
+
+        public int Size { get; set; }
+    }
+}
