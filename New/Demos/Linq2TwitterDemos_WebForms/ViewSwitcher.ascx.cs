83b7bac78cbf79c1d6f60227925da2b7545767f0
JoeMayo <jmayo@mayosoftware.com>
2014-11-04 11:46:33 -0800

    Updated NuGet assembly references.

--- New/Demos/Linq2TwitterDemos_WebForms/ViewSwitcher.ascx.cs
+++ New/Demos/Linq2TwitterDemos_WebForms/ViewSwitcher.ascx.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using Microsoft.AspNet.FriendlyUrls.Resolvers;
 
 namespace Linq2TwitterDemos_WebForms
