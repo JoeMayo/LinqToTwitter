7704dd2a197c6874581989773a63f75fcd0b27bd
Joe Mayo <jmayo@mayosoftware.com>
2014-11-29 16:59:44 -0800

    Created a new updated MVC 5 demo.

---
+++ New/Linq2TwitterDemos_Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Linq2TwitterDemos_Mvc.MvcApplication" Language="C#" %>
---
+++ New/Linq2TwitterDemos_Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Linq2TwitterDemos_Mvc.Startup))]
+namespace Linq2TwitterDemos_Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
