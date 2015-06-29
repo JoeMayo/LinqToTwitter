c71c56c64c2e4f788c5238ae15d7e865ab752b5a
Joe Mayo <jmayo@mayosoftware.com>
2015-06-28 22:59:00 -0700

    Added new Xamarin project with ModernHttpClient.

---
+++ src/LinqToTwitter/LinqToTwitter.Shared.xamarin/Net/PostMessageHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using ModernHttpClient;
+
+namespace LinqToTwitter.Net
+{
+    class PostMessageHandler : NativeMessageHandler
+    {
+        readonly TwitterExecute exe;
+        readonly IDictionary<string, string> postData;
+        readonly string url;
+
+        public PostMessageHandler(TwitterExecute exe, IDictionary<string, string> postData, string url)
+        {
+            this.exe = exe;
+            this.postData = postData;
+            this.url = url;
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            exe.SetAuthorizationHeader(HttpMethod.Post.ToString(), url, postData, request);
+            request.Headers.Add("User-Agent", exe.UserAgent);
+            request.Headers.ExpectContinue = false;
+            request.Headers.CacheControl = new CacheControlHeaderValue { NoCache = true };
+
+            if (SupportsAutomaticDecompression)
+                AutomaticDecompression = DecompressionMethods.GZip;
+            if (exe.Authorizer.Proxy != null && SupportsProxy)
+                Proxy = exe.Authorizer.Proxy;
+
+            //if (exe.ReadWriteTimeout != 0)
+            //    ReadWriteTimeout = exe.ReadWriteTimeout;
+
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
