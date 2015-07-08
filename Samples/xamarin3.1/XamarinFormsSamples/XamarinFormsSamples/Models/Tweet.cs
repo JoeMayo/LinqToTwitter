d88fb744533f5388131768c4aff48bd7531d1ce5
Joe Mayo <jmayo@mayosoftware.com>
2015-07-07 22:37:55 -0700

    Additional files to support Xamarin libraries and samples.

---
+++ Samples/xamarin3.1/XamarinFormsSamples/XamarinFormsSamples/Models/Tweet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinFormsSamples.Models
+{
+    public class Tweet
+    {
+        public ulong StatusID { get; set; }
+
+        public string ScreenName { get; set; }
+
+        public string Text { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
