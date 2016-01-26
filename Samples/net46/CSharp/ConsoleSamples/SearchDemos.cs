4e933eb0a0efdf1704d4f0eac80db6857e79df34
Joe Mayo <jmayo@mayosoftware.com>
2016-01-26 15:23:49 -0800

    Added paged search demo.

--- Samples/net46/CSharp/ConsoleSamples/SearchDemos.cs
+++ Samples/net46/CSharp/ConsoleSamples/SearchDemos.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Threading.Tasks;
 using LinqToTwitter;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Linq2TwitterDemos_Console
 {
@@ -23,6 +25,10 @@ namespace Linq2TwitterDemos_Console
                         Console.WriteLine("\n\tSearching...\n");
                         await DoSearchAsync(twitterCtx);
                         break;
+                    case '1':
+                        Console.WriteLine("\n\tSearching...\n");
+                        await DoPagedSearchAsync(twitterCtx);
+                        break;
                     case 'q':
                     case 'Q':
                         Console.WriteLine("\nReturning...\n");
@@ -40,6 +46,7 @@ namespace Linq2TwitterDemos_Console
             Console.WriteLine("\nSearch Demos - Please select:\n");
 
             Console.WriteLine("\t 0. Search");
+            Console.WriteLine("\t 1. Paged Search");
             Console.WriteLine();
             Console.WriteLine("\t Q. Return to Main menu");
         }
@@ -49,7 +56,7 @@ namespace Linq2TwitterDemos_Console
             string searchTerm = "\"LINQ to Twitter\" OR Linq2Twitter OR LinqToTwitter";
             //string searchTerm = "#ömer -RT -instagram news source%3Afoursquare";
 
-            var searchResponse =
+            Search searchResponse =
                 await
                 (from search in twitterCtx.Search
                  where search.Type == SearchType.Search &&
@@ -64,6 +71,62 @@ namespace Linq2TwitterDemos_Console
                         tweet.User.ScreenNameResponse,
                         tweet.User.UserIDResponse,
                         tweet.Text));
+        }
+
+        static async Task DoPagedSearchAsync(TwitterContext twitterCtx)
+        {
+            const int MaxSearchEntriesToReturn = 100;
+
+            string searchTerm = "twitter";
+
+            // oldest id you already have for this search term
+            ulong sinceID = 1;
+
+            // used after the first query to track current session
+            ulong maxID; 
+
+            var combinedSearchResults = new List<Status>();
+
+            List<Status> searchResponse =
+                await
+                (from search in twitterCtx.Search
+                 where search.Type == SearchType.Search &&
+                       search.Query == searchTerm &&
+                       search.Count == MaxSearchEntriesToReturn &&
+                       search.SinceID == sinceID
+                 select search.Statuses)
+                .SingleOrDefaultAsync();
+
+            combinedSearchResults.AddRange(searchResponse);
+            ulong previousMaxID = ulong.MaxValue;
+            do
+            {
+                // one less than the newest id you've just queried
+                maxID = searchResponse.Min(status => status.StatusID) - 1;
+
+                Debug.Assert(maxID < previousMaxID);
+                previousMaxID = maxID;
+
+                searchResponse =
+                    await
+                    (from search in twitterCtx.Search
+                     where search.Type == SearchType.Search &&
+                           search.Query == searchTerm &&
+                           search.Count == MaxSearchEntriesToReturn &&
+                           search.MaxID == maxID &&
+                           search.SinceID == sinceID
+                     select search.Statuses)
+                    .SingleOrDefaultAsync();
+
+                combinedSearchResults.AddRange(searchResponse);
+            } while (searchResponse.Any());
+
+            combinedSearchResults.ForEach(tweet =>
+                Console.WriteLine(
+                    "\n  User: {0} ({1})\n  Tweet: {2}",
+                    tweet.User.ScreenNameResponse,
+                    tweet.User.UserIDResponse,
+                    tweet.Text));
         }
     }
 }
