adefd8398373ec68531b003a30391053aefa6e27
Joe Mayo <jmayo@mayosoftware.com>
2017-12-23 16:45:43 -0800

    Marked Site and User streams as Obsolete.

--- src/LinqToTwitter/LinqToTwitter.Shared/Streaming/StreamingType.cs
+++ src/LinqToTwitter/LinqToTwitter.Shared/Streaming/StreamingType.cs
@@ -1,4 +1,6 @@
-﻿namespace LinqToTwitter
+﻿using System;
+
+namespace LinqToTwitter
 {
     public enum StreamingType
     {
@@ -20,11 +22,13 @@
         /// <summary>
         /// Activity for multiple users
         /// </summary>
+        [Obsolete("Twitter is deprecating this stream on Jun 19, 2018. See the Account Activity API for more info: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/overview")]
         Site,
 
         /// <summary>
         /// A single user's activity
         /// </summary>
+        [Obsolete("Twitter is deprecating this stream on Jun 19, 2018. See the Account Activity API for more info: https://developer.twitter.com/en/docs/accounts-and-users/subscribe-account-activity/overview")]
         User
     }
 }
