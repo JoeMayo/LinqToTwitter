b5be8e67a67e2de278f1a654c118bc96d2a23ca0
Michael Washington <webmaster@adefwebserver.com>
2020-06-20 16:15:34 -0700

    #184

---
+++ Samples/LinqToTwitter5/net48/CSharp/AspNetSamples/BlazorDemo/Services/BlazorAuthorizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using LinqToTwitter;
+
+namespace BlazorDemo.Services
+{
+    public class BlazorAuthorizer : AspNetAuthorizer
+    {
+        public HttpClient _httpClient;
+        private string _authUrl;
+
+        public BlazorAuthorizer(HttpClient client)
+        {
+            _httpClient = client;
+        }  
+
+        public async Task<ActionResult> BeginAuthorizationAsync(
+            Uri callback, Dictionary<string, string> parameters = null)
+        {
+            if (GoToTwitterAuthorization == null)
+                GoToTwitterAuthorization = authUrl => { _authUrl = authUrl; };
+
+            Callback = callback;
+
+            await base.BeginAuthorizeAsync(callback, parameters).ConfigureAwait(false);
+
+            return new RedirectResult(_authUrl,false);
+        }
+    }
+}
