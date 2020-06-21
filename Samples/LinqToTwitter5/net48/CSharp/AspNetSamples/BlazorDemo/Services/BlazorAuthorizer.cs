0fc0244b9efa15854c4b6dd9ac2197277a9d6435
Michael Washington <webmaster@adefwebserver.com>
2020-06-21 07:48:35 -0700

    Major refactoring to remove duplicate code

--- Samples/LinqToTwitter5/net48/CSharp/AspNetSamples/BlazorDemo/Services/BlazorAuthorizer.cs
+++ Samples/LinqToTwitter5/net48/CSharp/AspNetSamples/BlazorDemo/Services/BlazorAuthorizer.cs
@@ -5,18 +5,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 using LinqToTwitter;
+using Microsoft.AspNetCore.Http;
 
 namespace BlazorDemo.Services
 {
     public class BlazorAuthorizer : AspNetAuthorizer
     {
         public HttpClient _httpClient;
+        HttpContextAccessor _httpContextAccessor;
         private string _authUrl;
 
-        public BlazorAuthorizer(HttpClient client)
+        public BlazorAuthorizer(
+            HttpClient client, 
+            HttpContextAccessor HttpContextAccessor
+            )
         {
             _httpClient = client;
-        }  
+            _httpContextAccessor = HttpContextAccessor;
+        }
+
+        public string GetPathBase()
+        {
+            var request = _httpContextAccessor.HttpContext.Request;
+            var host = request.Host.ToUriComponent();
+            var pathBase = request.PathBase.ToUriComponent();
+            return $@"{request.Scheme}://{host}{pathBase}/";
+        }
+
+        public async Task<ActionResult> Begin()
+        {
+            this.CredentialStore = new InMemoryCredentialStore();
+            this.CredentialStore.ConsumerKey = Environment.GetEnvironmentVariable("TwitterConsumerKey");
+            this.CredentialStore.ConsumerSecret = Environment.GetEnvironmentVariable("TwitterConsumerSecret");
+
+            // to pass parameters that you can read in Complete(), via Request.QueryString, when Twitter returns
+            // var parameters = new Dictionary<string, string> { { "my_custom_param", "val" } };
+            // return await auth.BeginAuthorizationAsync(new Uri(GetPathBase()), parameters);
+
+            return (RedirectResult)await BeginAuthorizationAsync(new Uri(GetPathBase()));
+        }
 
         public async Task<ActionResult> BeginAuthorizationAsync(
             Uri callback, Dictionary<string, string> parameters = null)
