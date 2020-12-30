0bed8cf8dcb9cb3cd92774f44657e63992a26a5e
Joe Mayo <jmayo@mayosoftware.com>
2020-12-29 22:32:43 -0800

    Implemented TweetPoll.

--- src/LinqToTwitter6/LinqToTwitter/Tweet/TweetPoll.cs
+++ src/LinqToTwitter6/LinqToTwitter/Tweet/TweetPoll.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace LinqToTwitter
 {
+    /// <summary>
+    /// Represents a Twitter poll
+    /// </summary>
     public record TweetPoll
     {
+        /// <summary>
+        /// ID for the poll
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string? ID { get; init; }
+
+        /// <summary>
+        /// Poll options
+        /// </summary>
+        [JsonPropertyName("options")]
+        public List<TweetPollOption>? Options { get; init; }
+
+        /// <summary>
+        /// Number of minutes to run poll
+        /// </summary>
+        [JsonPropertyName("duration_minutes")]
+        public int DurationMinutes { get; init; }
+
+        /// <summary>
+        /// Ending date and time
+        /// </summary>
+        [JsonPropertyName("end_datetime")]
+        public DateTime EndDatetime { get; init; }
+
+        /// <summary>
+        /// Whether poll is open or closed
+        /// </summary>
+        [JsonPropertyName("voting_status")]
+        public string? VotingStatus { get; init; }
     }
 }
---
+++ src/LinqToTwitter6/LinqToTwitter/Tweet/TweetPollOption.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LinqToTwitter
+{
+    /// <summary>
+    /// An option in a Twitter poll
+    /// </summary>
+    public record TweetPollOption
+    {
+        /// <summary>
+        /// Ordinal of the option
+        /// </summary>
+        [JsonPropertyName("position")]
+        public int Position { get; init; }
+
+        /// <summary>
+        /// Text of option
+        /// </summary>
+        [JsonPropertyName("label")]
+        public string? Label { get; init; }
+
+        /// <summary>
+        /// How many people voted for this option
+        /// </summary>
+        [JsonPropertyName("votes")]
+        public int Votes { get; init; }
+    }
+}
