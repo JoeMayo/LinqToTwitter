a1131ceae9e408a048541ee484cd74db18450bf3
Joe Mayo <jmayo@mayosoftware.com>
2021-01-01 22:12:30 -0800

    Updates to Twitter place to match data dictionary.

--- src/LinqToTwitter6/LinqToTwitter/Common/TwitterPlace.cs
+++ src/LinqToTwitter6/LinqToTwitter/Common/TwitterPlace.cs
@@ -1,28 +1,56 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace LinqToTwitter
 {
     public record TwitterPlace
     {
+        /// <summary>
+        /// IDs of places containing this place
+        /// </summary>
+        [JsonPropertyName("contained_within")]
+        public List<string>? ContainedWithin { get; init; }
+
+        /// <summary>
+        /// Full-length country name
+        /// </summary>
         [JsonPropertyName("country")]
-        public string? Country { get; set; }
+        public string? Country { get; init; }
 
+        /// <summary>
+        /// ISO Alpha-2 country code
+        /// </summary>
         [JsonPropertyName("country_code")]
-        public string? CountryCode { get; set; }
+        public string? CountryCode { get; init; }
 
+        /// <summary>
+        /// Type of place - e.g. city or point of interest
+        /// </summary>
         [JsonPropertyName("place_type")]
-        public string? PlaceType { get; set; }
+        public string? PlaceType { get; init; }
 
+        /// <summary>
+        /// Short name of place
+        /// </summary>
         [JsonPropertyName("name")]
-        public string? Name { get; set; }
+        public string? Name { get; init; }
 
+        /// <summary>
+        /// Unique ID for this place
+        /// </summary>
         [JsonPropertyName("id")]
-        public string? ID { get; set; }
+        public string? ID { get; init; }
 
+        /// <summary>
+        /// Full name of place
+        /// </summary>
         [JsonPropertyName("full_name")]
-        public string? FullName { get; set; }
+        public string? FullName { get; init; }
 
+        /// <summary>
+        /// Place details
+        /// </summary>
         [JsonPropertyName("geo")]
-        public Geo? Geo { get; set; }
+        public Geo? Geo { get; init; }
     }
 }
