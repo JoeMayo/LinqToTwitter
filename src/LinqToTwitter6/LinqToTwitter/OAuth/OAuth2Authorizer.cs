10ebe65725319d990ba0f92268b28fd89983833f
Joe Mayo <jmayo@mayosoftware.com>
2021-11-10 20:02:45 -0800

    Working on OAuth2

---
+++ src/LinqToTwitter6/LinqToTwitter/OAuth/OAuth2Authorizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToTwitter.OAuth
+{
+    public class OAuth2Authorizer : AuthorizerBase, IAuthorizer
+    {
+        List<string> permissions;
+
+        public OAuth2Authorizer()
+        {
+            permissions = new List<string>();
+        }
+
+        public Task AuthorizeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
