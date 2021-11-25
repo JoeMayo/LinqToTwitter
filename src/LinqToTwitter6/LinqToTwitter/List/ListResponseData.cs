02de018e7e08384368e8cc254e2542624ae99d20
Joe Mayo <jmayo@mayosoftware.com>
2021-11-24 20:49:29 -0800

    Added a couple of new types to support lists

--- src/LinqToTwitter6/LinqToTwitter/List/ListResponseData.cs
+++ src/LinqToTwitter6/LinqToTwitter/List/ListResponseData.cs
@@ -1,14 +1,61 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 
 namespace LinqToTwitter
 {
     public record ListResponseData
     {
+        /// <summary>
+        /// When created
+        /// </summary>
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// What this list is about
+        /// </summary>
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Number of users following this list
+        /// </summary>
+        [JsonPropertyName("follower_count")]
+        public int FollowerCount { get; set; }
+
+        /// <summary>
+        /// List ID
+        /// </summary>
         [JsonPropertyName("id")]
         public string? ID { get; set; }
 
+        /// <summary>
+        /// Number of users are members in this list
+        /// </summary>
+        [JsonPropertyName("member_count")]
+        public int MemberCount { get; set; }
+
+        /// <summary>
+        /// List Name
+        /// </summary>
         [JsonPropertyName("name")]
         public string? Name { get; set; }
+
+        /// <summary>
+        /// ID of user who is the list owner
+        /// </summary>
+        [JsonPropertyName("owner_id")]
+        public string? OwnerID { get; set; }
+
+        /// <summary>
+        /// Can only the owner see this list?
+        /// </summary>
+        [JsonPropertyName("private")]
+        public bool Private { get; set; }
+
+        //
+        // The following properties are only returned for commands
+        //
 
         [JsonPropertyName("updated")]
         public bool Updated { get; set; }
