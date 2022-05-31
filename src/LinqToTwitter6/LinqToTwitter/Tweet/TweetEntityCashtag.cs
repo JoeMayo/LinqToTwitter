461c7909330ec320dda347ca551b79d57873a86d
Joe Mayo <jmayo@mayosoftware.com>
2022-05-30 22:31:02 -0700

    Implemented Cashtag.

---
+++ src/LinqToTwitter6/LinqToTwitter/Tweet/TweetEntityCashtag.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LinqToTwitter
+{
+    /// <summary>
+    /// Represents a hashtag entity
+    /// </summary>
+    public record TweetEntityCashtag
+    {
+        /// <summary>
+        /// Start of hashtag
+        /// </summary>
+        [JsonPropertyName("start")]
+        public int Start { get; init; }
+
+        /// <summary>
+        /// End of hashtag
+        /// </summary>
+        [JsonPropertyName("end")]
+        public int End { get; init; }
+
+        /// <summary>
+        /// Hashtag text
+        /// </summary>
+        [JsonPropertyName("tag")]
+        public string? Tag { get; init; }
+    }
+}
