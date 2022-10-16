4acbb88fbf551ab3a075b37e02b51c3f7c3c4163
Joe Mayo <jmayo@mayosoftware.com>
2022-10-16 15:53:00 -0700

    Starting a new Maui project with a MauiAuthorizer.

---
+++ src/LinqToTwitter6/LinqToTwitter.Maui/MauiAuthorizer.cs
@@ -0,0 +1,33 @@
+﻿using LinqToTwitter.OAuth;
+
+namespace LinqToTwitter.Maui
+{
+    public class MauiAuthorizer : OAuth2Authorizer
+    {
+        public new async Task AuthorizeAsync()
+        {
+            if (CredentialStore is not IOAuth2CredentialStore credStore)
+                throw new NullReferenceException(CredentialStoreMessage);
+
+            credStore.State = GenerateCodeChallenge();
+
+            if (string.IsNullOrWhiteSpace(credStore.ClientID))
+                throw new ArgumentException($"You must populate CredentialStore with a {nameof(credStore.ClientID)} before calling AuthorizeAsync.", nameof(credStore.ClientID));
+            if (string.IsNullOrWhiteSpace(credStore.RedirectUri))
+                throw new ArgumentException($"You must populate CredentialStore with a {nameof(credStore.RedirectUri)} before calling AuthorizeAsync.", nameof(credStore.RedirectUri));
+            if (!credStore.Scopes?.Any() ?? false)
+                throw new ArgumentException($"You must populate CredentialStore with {nameof(credStore.Scopes)} (permissions) before calling AuthorizeAsync.", nameof(credStore.Scopes));
+
+            string authUrl = PrepareAuthorizeUrl(credStore.State);
+
+            var authResult =
+                await WebAuthenticator.AuthenticateAsync(
+                    new Uri(authUrl),
+                    new Uri(credStore.RedirectUri));
+
+            await CompleteAuthorizeAsync(authResult.Properties["code"], authResult.Properties["state"]);
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ src/LinqToTwitter6/LinqToTwitter.Maui/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace LinqToTwitter.Maui
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
