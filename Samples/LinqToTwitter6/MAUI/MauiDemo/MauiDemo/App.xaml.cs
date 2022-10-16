32d06e86a4a733a303185a31c889eba1bf23b236
Joe Mayo <jmayo@mayosoftware.com>
2022-10-16 15:53:57 -0700

    Initial work on a MAUI demo app.

---
+++ Samples/LinqToTwitter6/MAUI/MauiDemo/MauiDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Samples/LinqToTwitter6/MAUI/MauiDemo/MauiDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
